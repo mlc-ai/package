b12ba715541e37cada69c8bce4c0f04d18821828
Zihao <expye@outlook.com>
2023-05-21 00:56:52 -0700

    remove orderedset dependency

--- vulkan/docker/install/almalinux_install_python_package.sh
+++ vulkan/docker/install/almalinux_install_python_package.sh
@@ -7,7 +7,7 @@ source /multibuild/manylinux_utils.sh
 eval "$(conda shell.bash hook)"
 
 PYTHON_PACKAGES="six numpy pytest cython decorator scipy tornado typed_ast mypy \
-orderedset antlr4-python3-runtime attrs requests Pillow packaging junitparser synr cloudpickle xgboost==1.5.0"
+antlr4-python3-runtime attrs requests Pillow packaging junitparser synr cloudpickle xgboost==1.5.0"
 
 for env in $(conda env list | grep py | awk '{print $1}'); do
     conda activate "$env"
