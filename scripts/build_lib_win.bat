042f6cf43e01d4c4cb6ec753d1ab536e90bca90d
Junru Shao <junrushao1994@gmail.com>
2023-06-03 16:40:03 -0500

    Update windows version (#16)

--- scripts/build_lib_win.bat
+++ scripts/build_lib_win.bat
@@ -6,7 +6,7 @@ mkdir build
 cd build
 
 cmake -A x64 -Thost=x64 ^
-      -G "Visual Studio 16 2019" ^
+      -G "Visual Studio 17 2022" ^
       -DUSE_LLVM="llvm-config --link-static" ^
       -DUSE_RPC=ON ^
       -DUSE_SORT=ON ^
