1fdacd83139b7f22314eff63ea7a0009c06cdbbe
Zihao <expye@outlook.com>
2023-06-11 18:56:06 -0700

    fix

--- docker/install/almalinux_install_vulkan_sdk.sh
+++ docker/install/almalinux_install_vulkan_sdk.sh
@@ -10,9 +10,9 @@ wget https://sdk.lunarg.com/sdk/download/${vulkan_version}/linux/vulkansdk-linux
 tar xf vulkansdk-linux-x86_64-${vulkan_version}.tar.gz -C ~/vulkan
 rm vulkansdk-linux-x86_64-${vulkan_version}.tar.gz
 export VULKAN_SDK=~/vulkan/${vulkan_version}/x86_64
-cp -r $VULKAN_SDK/include/vulkan/ /usr/local/include/
-cp -P $VULKAN_SDK/lib/libvulkan.so* /usr/local/lib/
-cp $VULKAN_SDK/lib/libVkLayer_*.so /usr/local/lib/
+cp -r $VULKAN_SDK/include/vulkan/ /usr/include/
+cp -P $VULKAN_SDK/lib/libvulkan.so* /usr/lib64/
+cp $VULKAN_SDK/lib/libVkLayer_*.so /usr/lib64/
 mkdir -p /usr/local/share/vulkan/explicit_layer.d
 cp $VULKAN_SDK/etc/vulkan/explicit_layer.d/VkLayer_*.json /usr/local/share/vulkan/explicit_layer.d
 
