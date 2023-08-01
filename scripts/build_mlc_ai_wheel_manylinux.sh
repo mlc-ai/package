18fade81532ee1f3a5c444dcc54e59a8a79f0cb1
Junru Shao <junrushao1994@gmail.com>
2023-08-01 12:31:47 -0700

    Enable CUTLASS in Linux wheels (#42)

--- scripts/build_mlc_ai_wheel_manylinux.sh
+++ scripts/build_mlc_ai_wheel_manylinux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source /multibuild/manylinux_utils.sh
+source /opt/rh/gcc-toolset-11/enable # GCC-11 is the hightest GCC version compatible with NVCC < 12
 
 function usage() {
     echo "Usage: $0 [--cuda CUDA]"
@@ -92,6 +93,7 @@ echo set\(USE_VULKAN ON\) >> config.cmake
 
 if [[ ${CUDA} != "none" ]]; then
     echo set\(USE_CUDA ON\) >> config.cmake
+    echo set\(USE_CUTLASS ON\) >> config.cmake
 fi
 
 # compile the tvm
