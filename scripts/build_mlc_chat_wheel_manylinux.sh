9fb55c3674227ce56b82907ff1c6f380f72c343c
Junru Shao <junrushao1994@gmail.com>
2023-10-03 16:59:11 -0400

    upd

--- scripts/build_mlc_chat_wheel_manylinux.sh
+++ scripts/build_mlc_chat_wheel_manylinux.sh
@@ -92,7 +92,7 @@ echo set\(USE_VULKAN ON\) >>config.cmake
 
 if [[ ${GPU} == rocm* ]]; then
 	echo set\(USE_ROCM ON\) >>config.cmake
-	echo set\(USE_RCCL ON\) >>config.cmake
+	echo set\(USE_RCCL /opt/rocm/rccl/ \) >>config.cmake
 elif [[ ${GPU} == cuda* ]]; then
 	echo set\(USE_CUDA ON\) >>config.cmake
 	echo set\(USE_CUTLASS ON\) >>config.cmake
