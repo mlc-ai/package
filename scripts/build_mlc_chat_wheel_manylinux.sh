5bae62c71e8c1bf4b66e43d6be75a58cc10ecea7
Junru Shao <junrushao1994@gmail.com>
2023-10-03 19:47:27 -0700

    Update Docker Image (#57)

--- scripts/build_mlc_chat_wheel_manylinux.sh
+++ scripts/build_mlc_chat_wheel_manylinux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 source /multibuild/manylinux_utils.sh
+source /opt/rh/gcc-toolset-11/enable # GCC-11 is the hightest GCC version compatible with NVCC < 12
 
 function usage() {
 	echo "Usage: $0 [--gpu GPU-VERSION]"
@@ -92,7 +93,7 @@ echo set\(USE_VULKAN ON\) >>config.cmake
 
 if [[ ${GPU} == rocm* ]]; then
 	echo set\(USE_ROCM ON\) >>config.cmake
-	echo set\(USE_RCCL ON\) >>config.cmake
+	echo set\(USE_RCCL /opt/rocm/rccl/ \) >>config.cmake
 elif [[ ${GPU} == cuda* ]]; then
 	echo set\(USE_CUDA ON\) >>config.cmake
 	echo set\(USE_CUTLASS ON\) >>config.cmake
