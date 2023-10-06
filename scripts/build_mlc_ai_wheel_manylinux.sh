2a23fc55bd2cc8943ce3a56eb565976c764d34c2
Junru Shao <junrushao1994@gmail.com>
2023-10-06 13:28:04 -0400

    Update LLVM with ROCm-LLM on AMD

--- scripts/build_mlc_ai_wheel_manylinux.sh
+++ scripts/build_mlc_ai_wheel_manylinux.sh
@@ -89,15 +89,16 @@ fi
 
 # config the cmake
 cd /workspace/tvm
-echo set\(USE_LLVM \"llvm-config --ignore-libllvm --link-static\"\) >>config.cmake
 echo set\(HIDE_PRIVATE_SYMBOLS ON\) >>config.cmake
 echo set\(USE_RPC ON\) >>config.cmake
 echo set\(USE_VULKAN ON\) >>config.cmake
 
 if [[ ${GPU} == rocm* ]]; then
+	echo set\(USE_LLVM \"/opt/rocm/llvm/bin/llvm-config --ignore-libllvm --link-static\"\) >>config.cmake
 	echo set\(USE_ROCM ON\) >>config.cmake
 	echo set\(USE_RCCL /opt/rocm/rccl/ \) >>config.cmake
 elif [[ ${GPU} == cuda* ]]; then
+	echo set\(USE_LLVM \"llvm-config --ignore-libllvm --link-static\"\) >>config.cmake
 	echo set\(USE_CUDA ON\) >>config.cmake
 	echo set\(USE_CUTLASS ON\) >>config.cmake
 	echo set\(USE_NCCL ON\) >>config.cmake
