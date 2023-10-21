f48f07c545ee4880ae11257a071342edb4aca304
Junru Shao <junrushao1994@gmail.com>
2023-10-20 17:29:07 -0700

    Add Docker for CI

---
+++ docker/install/ubuntu/core.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euxo pipefail
+
+export TZ=America/Los_Angeles
+echo $TZ >/etc/timezone
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime
+
+apt update
+apt --yes full-upgrade
+apt install --yes wget curl git zsh tmux htop vim ccache cmake subversion gdb build-essential unzip \
+	software-properties-common locales tzdata apt-transport-https openssh-server libgtest-dev \
+	python3-pip
+
+locale-gen "en_US.UTF-8"
