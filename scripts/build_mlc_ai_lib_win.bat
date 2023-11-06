ebe2cd9aa6c3db3a50026b94267e97f8d7ec8d4c
Junru Shao <junrushao1994@gmail.com>
2023-11-06 10:50:08 -0800

    Fix Windows Build with LLVM's Findzstd.cmake

--- scripts/build_mlc_ai_lib_win.bat
+++ scripts/build_mlc_ai_lib_win.bat
@@ -11,6 +11,8 @@ cmake -A x64 -Thost=x64 ^
       -DZLIB_USE_STATIC_LIBS=ON ^
       -DUSE_RPC=ON ^
       -DUSE_VULKAN=ON ^
+      -DCMAKE_INSTALL_LIBDIR=lib ^
+      -DCMAKE_INSTALL_BINDIR=bin ^
       ..
 
 cmake --build . --parallel 3 --config Release -- /m
