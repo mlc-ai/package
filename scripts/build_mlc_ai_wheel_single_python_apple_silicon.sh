56dd7ccf116ba6bde7e3ae1c1eb0b5112026bba1
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-02-07 18:51:17 -0500

    MLC-AI package stable release v0.15.1 wheel build (#70)

--- scripts/build_mlc_ai_wheel_single_python_apple_silicon.sh
+++ scripts/build_mlc_ai_wheel_single_python_apple_silicon.sh
@@ -5,6 +5,7 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 CONDA_ENV_NAME=$1
 deploy="${DEPLOY_WHEEL:-0}"
+stable="${STABLE_BUILD:-0}"
 
 source $CONDA_HOME/etc/profile.d/conda.sh
 
@@ -14,7 +15,11 @@ echo "Start build for $CONDA_ENV_NAME"
 conda activate $CONDA_ENV_NAME
 
 # sync package
-python scripts/sync_package.py --package tvm --package-name mlc-ai-nightly --revision origin/mlc --skip-checkout --skip-conda
+if [ "$stable" -eq "0" ]; then
+  python scripts/sync_package.py --package tvm --package-name mlc-ai-nightly --revision origin/mlc --skip-checkout --skip-conda
+else
+  python scripts/sync_package.py --package tvm --package-name mlc-ai --revision origin/mlc --skip-conda
+fi
 # build tvm
 ./scripts/build_mlc_ai_lib_apple_silicon.sh
 
