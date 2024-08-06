16b17814d7f83b5dab8af1a92a9dda0bd30a4f38
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-08-05 20:37:28 -0400

    Add ROCm 6.1/6.2; remove ROCm 5.6/5.7

---
+++ docker/install/almalinux_install_rocm_62.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+dnf install -y epel-release
+
+for ver in 6.2; do
+tee --append /etc/yum.repos.d/rocm.repo <<EOF
+[ROCm-$ver]
+name=ROCm$ver
+baseurl=https://repo.radeon.com/rocm/rhel8/$ver/main
+enabled=1
+priority=50
+gpgcheck=1
+gpgkey=https://repo.radeon.com/rocm/rocm.gpg.key
+EOF
+done
+
+dnf install -y rocm-hip-sdk
