b6b04f1a4ba08be00158ac62864c77c613d1e703
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-08-24 11:35:51 -0400

    CUDA module hotfix

--- docker/install/almalinux_install_cuda_122.sh
+++ docker/install/almalinux_install_cuda_122.sh
@@ -7,6 +7,7 @@ dnf install epel-release -y
 dnf update -y
 rpm -q epel-release
 yum config-manager --add-repo http://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+sed -i '2imodule_hotfixes=1' /etc/yum.repos.d/cuda-rhel8.repo
 dnf install kernel-devel -y
 dnf install cuda-12-2 -y
 
