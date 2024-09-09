677f7c9aaf00f6e566957255135af87e456f37e2
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-09-09 15:43:07 -0400

    Stable release v0.17.1

--- scripts/build_mlc_llm_wheel_single_python_apple_silicon.sh
+++ scripts/build_mlc_llm_wheel_single_python_apple_silicon.sh
@@ -5,6 +5,7 @@ eval "$(command conda 'shell.bash' 'hook' 2> /dev/null)"
 
 CONDA_ENV_NAME=$1
 deploy="${DEPLOY_WHEEL:-0}"
+stable="${STABLE_BUILD:-0}"
 
 source $CONDA_HOME/etc/profile.d/conda.sh
 
@@ -14,7 +15,11 @@ echo "Start build for $CONDA_ENV_NAME"
 conda activate $CONDA_ENV_NAME
 
 # sync package
-python scripts/sync_package.py --package mlc-llm --package-name mlc-llm-nightly --revision origin/main --skip-checkout --skip-conda
+if [ "$stable" -eq "0" ]; then
+  python scripts/sync_package.py --package mlc-llm --package-name mlc-llm-nightly --revision origin/main --skip-checkout --skip-conda
+else
+  python scripts/sync_package.py --package mlc-llm --package-name mlc-llm --revision origin/main --skip-conda
+fi
 # build mlc-llm
 ./scripts/build_mlc_llm_lib_apple_silicon.sh
 
