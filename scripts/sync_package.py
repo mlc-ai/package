cd9e5967ab5c358618f5f26c4d91108b600782ee
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-09-27 22:32:40 -0400

    Stable release v0.17.2 (#90)

--- scripts/sync_package.py
+++ scripts/sync_package.py
@@ -7,7 +7,7 @@ import subprocess
 # Modify the following two settings during release
 # -----------------------------------------------------------
 # Tag used for stable build.
-__stable_build__ = "v0.17.1"
+__stable_build__ = "v0.17.2"
 # -----------------------------------------------------------
 
 
