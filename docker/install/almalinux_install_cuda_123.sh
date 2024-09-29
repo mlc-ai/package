254d630ebef4c299622002d12d3a34588b870333
Ruihang Lai <ruihangl@cs.cmu.edu>
2024-09-28 21:37:47 -0400

    CUDA 12.3 package docker

---
+++ docker/install/almalinux_install_cuda_123.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+dnf install epel-release -y
+dnf update -y
+rpm -q epel-release
+yum config-manager --add-repo http://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo
+sed -i '2imodule_hotfixes=1' /etc/yum.repos.d/cuda-rhel8.repo
+dnf install kernel-devel -y
+dnf install cuda-12-3 -y
+
+NCCL_VERSION=$(dnf --showduplicates list libnccl | grep "cuda12.3" | tail -1 | awk '{print $2}')
+dnf install libnccl-$NCCL_VERSION libnccl-devel-$NCCL_VERSION libnccl-static-$NCCL_VERSION -y
