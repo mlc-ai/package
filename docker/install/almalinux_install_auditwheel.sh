a86c2a7cfd991df991d69dc368ed93c2c9f1a536
Ruihang Lai <ruihangl@cs.cmu.edu>
2026-04-20 10:50:49 -0400

    Update auditwheel install script

--- docker/install/almalinux_install_auditwheel.sh
+++ docker/install/almalinux_install_auditwheel.sh
@@ -7,4 +7,4 @@ source /multibuild/manylinux_utils.sh
 # use a forked version with skip-libs option
 git clone https://github.com/mlc-ai/auditwheel
 cd auditwheel
-python3 setup.py install
+pip install .
